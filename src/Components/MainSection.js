81b11f1203efb7addb179172a8d396a680ac6304
Matúš Kalanin <mato.kalanin@gmail.com>
2021-04-16 17:17:06 +0200

    heroku deployment

---
+++ src/Components/MainSection.js
@@ -0,0 +1,60 @@
+import React, { Component } from 'react';
+import style from "./MainSection.module.scss";
+import { Container, Row, Col } from 'react-bootstrap';
+import background_pic from '../assets/background.png';
+import coffee_pic from '../assets/coffee.svg';
+
+class MainSection extends Component{
+    render() {
+        return(
+                <div className={ style.main_header}>
+                    <div className={style.image_wrapper}>
+                        <img className={style.background_pic} src={background_pic}></img>
+                        <img className={style.coffee_pic} src={coffee_pic} ></img>
+                    </div>
+                    <div className={style.text_header_section}>
+                        <div className={style.headline_header}>
+                            <h2>
+                                FISH OIL
+                            </h2>
+                        </div>
+                        <div className={style.sub_headline_header}>
+                            <p>
+                                Type: <span> Supplements </span>
+                            </p>
+                        </div>
+                        <div className={style.product_header_categories}>
+                            <button>
+
+                                For non-fish eaters
+                            </button>
+                            <button>
+                                For your brain, heart, eyes
+                            </button>
+                        </div>
+                        <div className={style.product_information}>
+                            <h4>Product information</h4>
+                            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Tempor commodo ullamcorper a lacus vestibulum. Luctus accumsan tortor posuere ac ut. Vel pretium lectus quam id leo in vitae turpis.  
+                            </p>
+                        </div>
+                        <div className={style.product_price}>
+                            89 kr./md.
+                        </div>
+                        <div className={style.header_action}>
+                            <button className={style.button_test}>
+                                Do the health test
+                            </button>
+                            <button className={style.button_add}>
+                                <p> Add to cart</p>
+                            </button>
+                        </div>
+                    </div>
+                </div>
+                    )
+    }
+}
+
+export default MainSection;
+
+/* Fish_Oil 1 */
+
---
+++ src/Components/ResponsiveBoxes.js
@@ -0,0 +1,65 @@
+import React, {Component} from 'react';
+import style from "./MainSection.module.scss";
+import '../../node_modules/bootstrap/dist/css/bootstrap.min.css';
+import { Container, Row, Col } from 'react-bootstrap';
+import eye from '../assets/icons/eye.png';
+
+class ResponsiveBoxes extends Component{
+    cards = [
+        {
+            "image": '/assets/icons/eye.png',
+            "headline" : "Eyes",
+            "text" : "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt"
+        },
+        {
+            "image": '/assets/icons/heart.svg',
+            "headline" : "Heart",
+            "text" : "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt"
+        },
+        {
+            "image": '/assets/icons/brain.svg',
+            "headline" : "Brain Capacity",
+            "text" : "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt"
+        },
+        {
+            "image": '/assets/icons/quality.svg',
+            "headline" : "High Quality",
+            "text" : "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt"
+        },
+        {
+            "image": '/assets/icons/sustainable.svg',
+            "headline" : "Sustainable",
+            "text" : "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt"
+        }
+    ]
+    render(){
+        return (
+            <Container >
+                <Row className={style.card_wrapper}>
+                    {
+                        this.cards.map((card) => (
+                       
+                    <Col lg={2} md={3} sm={6} xs={12} className={style.card}>
+                        <div className={style.card_image_wrapper}>
+                            <img className={style.card_image} src={card.image}></img>
+                        </div>
+                        <div className={style.card_header}>
+                            <h2>
+                                {card.headline}
+                            </h2>
+                        </div>
+                        <div className={style.card_text}>
+                            <p>
+                                {card.text}
+                            </p>
+                        </div>
+                    </Col>
+                    ))}
+                </Row>
+                
+            </Container>
+        )
+    }
+}
+
+export default ResponsiveBoxes;
